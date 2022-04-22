5f9974ba00de18e552f2e78ce0cb51440308d14a
Danimal <chao-mu@hackpoetic.com>
2022-04-22 10:44:04 -0400

    backup checkin

--- run-db.lua
+++ run-db.lua
@@ -9,15 +9,16 @@ dofile("db.lua")
 a_vid = Video("assets/animals/free/underwater/Pexels Videos 2558530.mp4-SHRINK-REV.mp4")
 b_vid = Video("assets/animals/free/underwater/Video (1).mp4-SHRINK-REV.mp4")
 
+disable = {}
 function onControl(device, control, value)
     if value < 0.5 then
         return
     end
 
-    print(control)
+    disable[control] = not disable[control]
 
     if control == "bank_1_button_1" then
-        pipeline_idx  = next_index(pipeline_idx, 1, pipelines)
+        --pipeline_idx  = next_index(pipeline_idx, 1, pipelines)
     end
 end
 
@@ -28,27 +29,29 @@ function render()
     copy_into("0", 0)
 
     local controls = {}
-    for i=1,1 do
+    for i=1,4 do
         for j=1,16 do
-            table.insert(controls, params["bank_" .. i .. "_knob_" .. j])
+    --        table.insert(controls, params["bank_" .. i .. "_knob_" .. j])
+            local v = 1
+            if disable["bank_" .. i .. "_button_" .. j] then
+                v = 0
+            end
+
+            table.insert(controls, v)
         end
     end
 
     local calls = pipelines[pipeline_idx]
 
+    local new_calls = {}
+
+    local mixables = 0
     for i, call in ipairs(calls) do
-        local inputs = call.inputs
-        local mix = inputs["mix"]
-        if #controls > 0 then
-            if type(mix) == "string" then
-                local control = table.remove(controls)
-                inputs["mix"] = value(mix, control)
-            elseif type(mix) == "table" then
-                local control = table.remove(controls)
-                inputs["mix"].amp = control
-            end
+        local control = table.remove(controls)
+        if control > 0.5 then
+            table.insert(new_calls, call)
         end
     end
 
-    rend_call_stack(calls)
+    rend_call_stack(new_calls)
 end
